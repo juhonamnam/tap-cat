b171bc48999909611731cc63a3d5c21180276caf
juhonamnam <jhnamnam@gmail.com>
2022-06-29 20:37:29 +0900

    modify logger: mount logger in container

--- deploy/up.sh
+++ deploy/up.sh
@@ -1,7 +1,7 @@
-. ./deploy/version.sh
+# down
+. ./deploy/down.sh
 
-docker stop $(docker ps -q --filter ancestor=$APP_NAME:$APP_VERSION)
-docker rm $(docker ps -a -q --filter ancestor=$APP_NAME:$APP_VERSION)
-docker rmi -f $APP_NAME:$APP_VERSION
+# up
+mkdir -p logs
 docker build -f Dockerfile.production --tag $APP_NAME:$APP_VERSION .
-docker run --name $APP_NAME -d $APP_NAME:$APP_VERSION
+docker run --name $APP_NAME -v $(pwd)/logs:/app/logs -d $APP_NAME:$APP_VERSION production
