fc086a4408f579d9bd089ecabe8b4763c0b658a8
juhonamnam <jhnamnam@gmail.com>
2022-06-30 16:03:55 +0900

    modify deploy script

--- deploy/down.sh
+++ deploy/down.sh
@@ -1,5 +1,7 @@
+# version
 . ./deploy/version.sh
 
+# down
 docker stop $(docker ps -q --filter ancestor=$APP_NAME:$APP_VERSION)
 docker rm $(docker ps -a -q --filter ancestor=$APP_NAME:$APP_VERSION)
 docker rmi -f $APP_NAME:$APP_VERSION
--- deploy/up.sh
+++ deploy/up.sh
@@ -1,7 +1,17 @@
+# version
+. ./deploy/version.sh
+
 # down
 . ./deploy/down.sh
 
 # up
 mkdir -p logs
-docker build -f Dockerfile.production --tag $APP_NAME:$APP_VERSION .
-docker run --name $APP_NAME -v $(pwd)/logs:/app/logs -d $APP_NAME:$APP_VERSION production
+
+docker build \
+    -f Dockerfile.production \
+    --tag $APP_NAME:$APP_VERSION .
+
+docker run \
+    --name $APP_NAME \
+    -v $(pwd)/logs:/app/logs \
+    -d $APP_NAME:$APP_VERSION production
