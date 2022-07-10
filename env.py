c3c456358657641a0a2dffeac89e8440397b33e0
juhonamnam <jhnamnam@gmail.com>
2022-07-10 19:39:33 +0900

    fix: env variable error

--- env.py
+++ env.py
@@ -4,5 +4,5 @@ import os
 access_key = os.getenv('UPBIT_ACCESS_KEY')
 secret_key = os.getenv('UPBIT_SECRET_KEY')
 api_key = os.getenv('TELE_KEY')
-user_id = os.getenv('USER_ID')
+user_id = os.getenv('TELE_USER_ID')
 is_production = len(sys.argv) > 1 and sys.argv[1] == 'production'
